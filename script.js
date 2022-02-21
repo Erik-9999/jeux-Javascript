9515def0ff5dd519ab212ff9b73f05f3d7dd81cb
huguet E <ehugllo@gmail.com>
2022-02-21 12:48:56 +0100

    modification gamePlay add winner

--- script.js
+++ script.js
@@ -3,17 +3,12 @@ $(function () {
     let throws, scores, activePlayer, notActive, gamePlaying;
     
     let iconSelectPlayer = "<i class='bi '><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-record-fill' viewBox='0 0 16 16'><path fill-rule='evenodd' d='M8 13A5 5 0 1 0 8 3a5 5 0 0 0 0 10z'/></svg></i>"; 
-    // init
+
     $('.btn-new').click(function(e){
         e.preventDefault();
         alert('Veuillez selectionner un joueur puis clicker sur Roll Dice pour lancer le dé');
         init();
     });
-   
-    $('.btn-new').click(init);
-    $('.rules').click(function(){
-        alert('les regles sont simples, le premier joueur arrivé à 100 points à gagner. Cliquer sur \'Player\' pour changer de joueur, si tirez un 1 au dé et vous avez perdu');
-    })
 
     // function next player
     function selectPlayer(){
@@ -45,7 +40,19 @@ $(function () {
         $('.dice').show();
         $('#rollDice').show();
         $('#skip').show();
-    }      
+    } 
+    
+    
+    function winner(){        
+        $('.infoGame').append("<div class='showinfo col-6'>Player "+(activePlayer+1)+" vous avez Gagné !!</div>");
+                $('#player-1').removeClass('active').removeClass('clicked').removeAttr('style');
+                $('#player-1 i').remove() ;
+                $('#player-2').removeClass('active').removeClass('clicked').removeAttr('style'); 
+                $('#player-2 i').remove() ; 
+                $('.dice').delay(1000).hide(400);
+                $('#rollDice').delay(1000).hide(400);
+    }
+    
 
     //function END
     function end(){
@@ -104,16 +111,24 @@ $(function () {
             if (dice !== 1 && scores[activePlayer]<100) {                
                 scores[activePlayer] += dice;
                 throws[activePlayer]++;            
-
+                
                 $('#score-' + activePlayer).text(scores[activePlayer]); 
-                $('#current-'+ activePlayer).text(throws[activePlayer]);    
-           
+                $('#current-'+ activePlayer).text(throws[activePlayer]);               
+            
             }else{
-                // end game
-                end();
-            }            
+                // end game                    
+                end();          
+            }  
+            if(scores[activePlayer]>=100) {
+                winner();
+            }        
+                       
         }    
     })
     
-    
+    // init
+    $('.btn-new').click(init);
+    $('.rules').click(function(){
+        alert('les regles sont simples, le premier joueur arrivé à 100 points à gagner. Cliquer sur \'Player\' pour changer de joueur, si tirez un 1 au dé et vous avez perdu');
+    })
 }) 
