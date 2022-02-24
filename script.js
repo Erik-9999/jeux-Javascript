2a53ea35b0cf494dfb90f477badd7981f407a4ad
huguet E <ehugllo@gmail.com>
2022-02-24 10:54:55 +0100

    finalisation complete du jeu

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(function () {
     
     // Operating variable
 
-    let throws, scores, activePlayer, notActive, gamePlaying;
+    let scores, activePlayer, notActive, gamePlaying;
     
     let iconSelectPlayer = "<i class='bi '><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-record-fill' viewBox='0 0 16 16'><path fill-rule='evenodd' d='M8 13A5 5 0 1 0 8 3a5 5 0 0 0 0 10z'/></svg></i>"; 
 
@@ -10,7 +10,7 @@ $(function () {
 
     $('.btn-new').click(function(e){
         e.preventDefault();
-        alert('Veuillez selectionner un joueur puis clicker sur Roll Dice pour lancer le dé');
+        alert('Clicker sur Roll Dice pour lancer le dé');
         init();
     });
 
@@ -26,38 +26,13 @@ $(function () {
 
     function winner(){        
         $('.infoGame').append("<div class='showinfo col-6'>Player "+(activePlayer+1)+" vous avez Gagné !!</div>");              
-                $('.dice').delay(1000).hide(400);
-                $('#rollDice').delay(1000).hide(400);
+        $('.dice').delay(1000).hide(400);
+        $('#rollDice').delay(1000).hide(400);
+        $('.hold').delay(1000).hide(400);
+        gamePlaying = false;
     }  
-
-    // select player
-
-    $('#player-0').click(function(){ 
-        activePlayer = 0;
-        notActive =1;
-
-        if(!$(this).hasClass('clicked')) {
-            $(this).removeAttr('style');          
-            $('#player-0').addClass('active').addClass('clicked');            
-            selectPlayer(); 
-            }                      
-            $('#player1 i').remove();                    
-        $('#player-1').removeClass('clicked');        
-    });
-
-    $('#player-1').click(function(){ 
-        activePlayer = 1 ;
-        notActive=0;        
-        if(!$(this).hasClass('clicked')) { 
-            $(this).removeAttr('style');            
-            $('#player-1').addClass('active').addClass('clicked');             
-            selectPlayer();          
-            }                 
-            $('#player0 i').remove();                
-        $('#player-0').removeClass('clicked');        
-     }); 
-
-   
+    
+    
     // Dice roll
 
     $('.btn-roll').click( function() {
@@ -70,63 +45,54 @@ $(function () {
             diceDOM.attr('src','Images/d-' + dice + '.png');              
     
             //3. Update  score IF the rolled number was NOT a 1
-            if (dice !== 1 && scores[activePlayer]<100) {                
-                scores[activePlayer] += dice;
-                throws[activePlayer]++;            
-                
-                $('#score-' + activePlayer).text(scores[activePlayer]); 
-                $('#current-' + activePlayer).text(throws[activePlayer]);               
+            if (dice !== 1) {                
+                currents[activePlayer] += dice;                             
+                $('#current-' + activePlayer).text(currents[activePlayer]);               
             
-            }else{                
-                 nextPlayer();          
+            }else{             
+                nextPlayer();          
             }    
             if (scores[activePlayer] >= 100) {
                 winner();
-             }                 
+            }                 
         }    
     })
 
     // button hold
 
    $('.btn-hold').click(function() {
-        if (gamePlaying) {
-            // Add CURRENT score to GLOBAL score
-            scores[activePlayer] >=100;
+       
+       scores[activePlayer] += currents[activePlayer];
+       $('#score-' + activePlayer).text(scores[activePlayer]);
 
-            // Update the UI
-            $('#score-' + activePlayer).text(scores[activePlayer]);
-    
-            // Check if player won the game
-            if (scores[activePlayer] >= 100) {              
-                
-                $('.dice').css('display' , 'none'); 
-                $('#player'+activePlayer).append(iconSelectPlayer);               
-               
-                gamePlaying = false;
-            } else {
-                //Next player
-                nextPlayer();
-            }
-        }
+       if (scores[activePlayer] >= 100) { 
+           winner();
+       }else{
+           nextPlayer();
+       }
     });
     
     // function nextplayer 
 
     function nextPlayer() {        
         //Next player
+        currents[activePlayer]=0;
+        $('#current-' + activePlayer).text(currents[activePlayer]);
+
         activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
         notActive === 0 ? notActive = 1 : notActive = 0;       
-            selectPlayer()
-            $('#player-' + activePlayer).toggleClass('active ').toggleClass('clicked');            
-            $('#player-'+ notActive).toggleClass('active ').toggleClass('clicked');             
+        selectPlayer()
+        $('#player-' + activePlayer).toggleClass('active ').toggleClass('clicked');            
+        $('#player-'+ notActive).toggleClass('active ').toggleClass('clicked');             
     }    
 
     // function INIT
 
     function init() {
         scores = [0, 0];
-        throws= [0,0];
-        activePlayer = 0;               
+        currents = [0, 0];        
+        activePlayer = 0;
+        notActive = 1;              
         gamePlaying = true;        
         $('.dice').css('display' , 'none');
         $('#player-0').removeClass('active').removeClass('clicked').removeAttr('style');
@@ -151,6 +117,6 @@ $(function () {
     $('.btn-new').init();
 
     $('.rules').click(function(){
-        alert('Les regles sont simples, le premier joueur arrivé à 100 points à gagner. Cliquer sur \'Player\' ou \'hold\' pour changer de joueur, si tirez un 1 au dé il y a automatiquement un changement de joueur');
+        alert('Les regles sont simples, le premier joueur arrivé à 100 points à gagner. Cliquer sur \'hold\' pour changer de joueur, si tirez un 1 au dé vous perdez votre tour de jeu');
     })
 }) 
